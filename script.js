812808b9f19577134e0e9d7a155439c4c80c5aec
Sherin <68326500+Sherinsedalo@users.noreply.github.com>
2024-07-17 14:34:48 +0200

    changing the favicon and adding the API

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let apiQuotes = [];
 
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from the apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+// Get Quotes From API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// on load
+getQuotes();
